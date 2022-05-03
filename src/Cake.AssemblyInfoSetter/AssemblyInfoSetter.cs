52d35de86a2a6ac3907202b3126316bbcbcddd6b
Paul Milbank <paulmilbank@gmail.com>
2022-05-03 22:39:44 +1200

    Write some files to the console with the Cake addin library

--- src/Cake.AssemblyInfoSetter/AssemblyInfoSetter.cs
+++ src/Cake.AssemblyInfoSetter/AssemblyInfoSetter.cs
@@ -1,6 +1,19 @@
-﻿namespace Cake.AssemblyInfoSetter;
+﻿using Cake.Core;
+using Cake.Core.IO;
+using Cake.Core.Annotations;
 
-public sealed class AssemblyInfoSetter
+namespace Cake.AssemblyInfoSetter;
+
+[CakeAliasCategory("AssemblyInfoSetter")]
+public static class AssemblyInfoSetter
 {
-    
+    [CakeMethodAlias]
+    public static void SetAssemblyInfo(this ICakeContext context, string globPattern) 
+    {
+        var files = context.Globber.GetFiles(globPattern);
+
+        foreach (var file in files) {
+            Console.WriteLine(file.FullPath.ToString());
+        }
+    }
 }
